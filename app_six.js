22237a1da3ee001b5dd9819a1ab415803c558a0c
Abhishek Tiwari <tiwariabhishek27611@gmail.com>
2024-01-07 22:37:40 +0530

    node js part - 9

---
+++ app_six.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+
+const filePath = path.join(__dirname, 'pages')
+//const filePath1 = path.join(__dirname, "view");
+app.get("/", (req, res) => {
+  res.sendFile(`${filePath}/index.html`);
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(`${filePath}/about.html`);
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(`${filePath}/contact.html`);
+});
+app.get("/login", (req, res) => {
+  res.sendFile(`${filePath}/login.html`);
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(`${filePath}/404.html`);
+});
+
+app.set("view engine","ejs")
+
+app.get("/profile", (req, res) => {
+//   res.sendFile(`${filePath}/404.html`);
+const user ={
+    name:"Abhishek Tiwari",
+    email:"abhishek@gmail.com",
+    address:"HSR Layout",
+    phone:"123-333-485",
+    Role: "SDE-1"
+}
+  app.render('profile',{user});
+});
+
+
+app.listen(5000)
