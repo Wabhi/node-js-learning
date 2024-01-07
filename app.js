c64dee02e7f46b2da8909435914b168aa4b7bfd4
Abhishek Tiwari <tiwariabhishek27611@gmail.com>
2024-01-07 20:28:58 +0530

    node js part - 5

---
+++ app.js
@@ -0,0 +1,38 @@
+// console.log("abhhhh1")
+// setTimeout(()=>{
+//     console.log("abhhhh4")
+// },3000)
+
+// console.log("abhhhh2");
+// console.log("abhhhh3");
+
+// let a = 20;
+// let b =  0;
+
+// // setTimeout(()=>{
+// //     b=30
+// // },2000)
+// // console.log(a+b)
+
+// // const waitData = new Promise((resolve, reject) =>{
+// //     setTimeout(()=>{
+// //         b= 30;
+// //         resolve(30)
+// //     },3000)
+// // })
+
+// // waitData.then((data)=>{
+// //     console.log(a+data)
+// // })
+
+// co
+
+
+console.log("process started..........")
+setTimeout(()=>{
+    console.log("process runing after 2sec....")
+},2000)
+setTimeout(()=>{
+    console.log("process runing after 0sec....")
+})
+console.log("process finished.........")
